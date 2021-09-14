6d363fe9c719a5cce941bf59b3acf9127045fa6f
Daryl Wong <youcanfinddaryl@gmail.com>
2021-09-14 16:27:10 +0800

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <header>
             <nav>
-                <div className="flex gap-2 align-middle bg-green-100 ">
-                    <div className="grid grid-cols-2 gap-x-4 ml-12 my-3">
+                <div className="flex gap-2 bg-green-100 ">
+                    <div className="grid grid-cols-2 mx-20 my-3 w-full h-full">
                         <div className=""><Link to='/home'>FoodPrint</Link></div>
-                        <div className="justify-right"><Link to='/about'>About Us</Link></div>
+                        <div className="justify-self-end ">
+                            <Link to='/about' className="mx-2">About Us</Link>
+                            <Link to='/login' className="mx-2">Log In</Link>
+                            <Link to='/register' className="mx-2">Register</Link>
+                        </div>
                     </div>
                 </div>
             </nav>
