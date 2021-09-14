9064b509f8c77b8eb7c475426e6ca66ff2f181dc
Daryl Wong <youcanfinddaryl@gmail.com>
2021-09-14 16:29:29 +0800

    added links in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/Header'
 import {Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Route exact path = "/home" component={Home}/>
       <Route exact path = "/about" component={About}/>
+      <Route exact path = "/login" component={Login}/>
+      <Route exact path = "/register" component={Register}/>
 
     </div>
   );
