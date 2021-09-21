d6f64f4729c27c750be1e4e78a39e51082110578
yvonnelhs <yvonnefghijk@gmail.com>
2021-09-21 18:00:18 +0800

    added restaurants on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAppContext } from '../lib/contextLib'
 import LogInService from '../services/LogInService'
 import "animate.css"
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import TrendingRestaurant from '../components/TrendingRestaurant'
+import RestaurantService from '../services/RestaurantService'
 
 const Home = () => {
     const { isAuthenticated } = useAppContext()
@@ -26,26 +28,27 @@ const Home = () => {
     }, [controls, inView]);
 
     const fadeInRight = {
-        hidden: { x: "300px", 
-            opacity: 0  
+        hidden: {
+            x: "300px",
+            opacity: 0
         },
-        
+
         visible: {
             x: "0px",
-            scale: 1,
             opacity: 1,
             transition: {
                 duration: 0.5
             }
         }
-        
+
     }
 
     const fadeInLeft = {
-        hidden: { x: "-300px", 
-            opacity: 0  
+        hidden: {
+            x: "-300px",
+            opacity: 0
         },
-        
+
         visible: {
             x: "0px",
             scale: 1,
@@ -54,8 +57,17 @@ const Home = () => {
                 duration: 0.5
             }
         }
-        
+
     }
+
+    const [restaurants, setRestaurants] = useState([])
+    
+    useEffect(() => {
+        RestaurantService.getRestaurants().then((response) => {
+            console.log(response)
+            setRestaurants(response.data)
+        })
+    }, [])
 
     return (
         <div>
@@ -75,17 +87,37 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <div className="grid md:grid-cols-2 gap-4 md:pt-10 bg-white-standard h-screen">
-                        <motion.div ref={ref} className="Box" initial="hidden" animate={controls} variants={fadeInLeft}>  
-                            <img className = "transform scale-75" src="/images/landingPage2.webp" alt="cooking illustration" />
+                    <div className="grid md:grid-cols-2 gap-4 md:pt-10 bg-white-standard md:pb-16">
+                        <motion.div ref={ref} className="Box" initial="hidden" animate={controls} variants={fadeInLeft}>
+                            <img className="transform scale-75" src="/images/landingPage2.webp" alt="cooking illustration" />
                         </motion.div>
-                        <motion.div ref={ref} className="Box" initial="hidden" animate={controls} variants={fadeInRight}>
+                        <motion.div ref={ref} initial="hidden" animate={controls} variants={fadeInRight}>
                             <h1 className="text-4xl md:pt-32 md:pr-64 pl-1 font-bold">Reduce food waste and Save money</h1>
                             <h2 className="md:pt-4 md:pr-44 text-grey-light">Here at foodPrint, we encourage restaurant reservation to help F&B establishments reduce food waste. </h2>
                             <div className="flex justify-left">
                                 <Link to="/restaurants" className="border border-gray-200 mt-4 pt-2 pb-3 px-5 text-center text-green-standard hover:shadow-lg rounded-3xl">Discover Restaurants</Link>
                             </div>
                         </motion.div>
+                    </div>
+
+                    <div className="md:pl-24 md:pt-10 bg-white-standard md:pb-32 ">
+                        <h1 className="text-4xl md:pr-64 pl-1 font-bold pb-7" >What's Trending</h1>
+                            <div className=" flex flex-row gap-x-10 w-11/12 h-96 overflow-x-scroll">
+                                {/* <div>
+                                    <img className="rounded-xl object-contain" width="240" height="330"  src="/images/restaurant.jpg" alt="restaurant" />
+                                    <h2 className="text-grey-standard text-lg md:pt-3 pl-1">Bread Palace Cafe</h2>
+                                    <h3 className="text-grey-light text-base md:pt-1 pl-1">Serangoon</h3>
+                                </div> */}
+                                {/* <TrendingRestaurant name="Bread Palace Cafe" location="Serangoon" src="/images/restaurant.jpg" /> */}
+                                
+                                {
+                                    restaurants.map(restaurant => 
+                                        <TrendingRestaurant  name={restaurant.restaurantName} location={restaurant.restaurantLocation} src="/images/restaurant.jpg" />
+                                    )
+                                }
+
+                            </div>
+
                     </div>
                 </>
             ) : (
