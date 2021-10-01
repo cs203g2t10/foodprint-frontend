166ff4fa87266b13f57cf890dc84db8e1cb2eaf5
yvonnelhs <yvonnefghijk@gmail.com>
2021-10-01 15:07:39 +0800

    error page

--- src/components/errors/NotFound.js
+++ src/components/errors/NotFound.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div className="h-min-screen">
-            <div>
-                    <img className = "flex mx-auto"style = {{height:520}}src="/images/error.webp" alt="404" />
+        <div className="min-h-screen">
+            <div className="grid grid-col-2">
+                <div>
+                    <img className = "my-0 flex mx-auto"style = {{height:490}}src="/images/error.webp" alt="404" />
+                </div>
+                <div>
                     <h1 className="text-8xl text-center mb-3 text-grey-standard">oops!</h1>
                     <h1 className="text-2xl text-center text-grey-standard">looks like the page is not found</h1>
+                </div>
             </div>
         </div>
     )
