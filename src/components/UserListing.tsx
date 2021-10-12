0d628cfdf03f7c85273f609de6937d3be73d1941
Daryl Wong <youcanfinddaryl@gmail.com>
2021-10-12 15:30:37 +0800

    fix user management

--- src/components/UserListing.tsx
+++ src/components/UserListing.tsx
@@ -21,20 +21,21 @@ const UserListing = (props: any) => {
 
 
     return (
-        <div className="mx-10 flex gap-x-10">
-            <button className="flex-col border px-2 rounded bg-red-standard text-white-standard" onClick={() => { deleteUser(props.id) }}>X</button>
+        <div className="mx-10 grid grid-cols-12 gap-x-6">
+            <button className="border px-2 rounded bg-red-standard text-white-standard text-center" onClick={() => { deleteUser(props.id) }}>X</button>
+            <div className="text-center">{props.id}</div>
             {(edit ? <>
                 {/* <button className="flex-col border px-2 rounded bg-red-standard text-white-standard" onClick={() => { deleteUser(props.id) }}>Delete</button> */}
-                <p>{props.id}</p>
-                <input className="flex-col border pl-2 rounded" onChange={(e)=>{setEmail(e.target.value)}} placeholder={email}/>
-                <input className="flex-col border pl-2 rounded" onChange={(e)=>{setFirstName(e.target.value)}} placeholder={firstName}/>
-                <input className="flex-col border pl-2 rounded" onChange={(e)=>{setLastName(e.target.value)}} placeholder={lastName}/>
-                <input className="flex-col border pl-2 rounded" onChange={(e)=>{setRoles(e.target.value)}} placeholder={roles}/>
+                {/* <p>{props.id}</p> */}
+                <input className="flex-col border pl-2 rounded col-span-3" onChange={(e)=>{setEmail(e.target.value)}} placeholder={email}/>
+                <input className="flex-col border pl-2 rounded col-span-1" onChange={(e)=>{setFirstName(e.target.value)}} placeholder={firstName}/>
+                <input className="flex-col border pl-2 rounded col-span-1" onChange={(e)=>{setLastName(e.target.value)}} placeholder={lastName}/>
+                <input className="flex-col border pl-2 rounded col-span-2" onChange={(e)=>{setRoles(e.target.value)}} placeholder={roles}/>
                 <button className="flex-col border px-2 rounded bg-green-standard text-white-standard hover:shadow" 
                 onClick={() => {
                     updateUserDetails(props.id, email,firstName,lastName,roles)
                     setEdit(false);
-                    }}>Confirm changes</button>
+                    }}>Confirm</button>
                 <button className="flex-col border px-2 rounded bg-yellow-standard hover:shadow" 
                 onClick={() => {
                     setEmail(props.email);
@@ -47,10 +48,11 @@ const UserListing = (props: any) => {
             </>
                 : <>
                     {/* <button className="flex-col border px-2 rounded bg-red-standard text-white-standard" onClick={() => { deleteUser(props.id) }}>X</button> */}
-                    <div>{props.id}</div>
-                    <p className="flex-col">{email}</p>
-                    <p>{firstName} {lastName} </p>
-                    <p>{roles}</p>
+                    {/* <div className="text-center">{props.id}</div> */}
+                    <p className="col-span-3">{email}</p>
+                    <p className="col-span-1">{firstName}</p>
+                    <p className="col-span-1"> {lastName}</p>
+                    <p className="col-span-2">{roles}</p>
                     <button className="flex-col border px-2 rounded bg-green-standard text-white-standard hover:shadow" onClick={() => setEdit(true)}>Edit</button>
                 </>)}
 
