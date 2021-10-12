15e0ab20f81a1f29062aaac81a05585a1a6e37bb
Daryl Wong <youcanfinddaryl@gmail.com>
2021-10-12 20:41:25 +0800

    improved pagination color coding

--- src/components/PageLinks.tsx
+++ src/components/PageLinks.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 
-const PageLinks = (props:any) => {
-    const {numPages, currPage, setCurrPage} = props;
+const PageLinks = (props: any) => {
+    const { numPages, currPage, setCurrPage } = props;
 
     return (
         <div className="flex justify-center gap-x-3 mt-4">
-            Page: 
+            Page:
             {
-                [...Array(numPages)].map((x, i) => 
-                    <button key={i} className="hover:text-blue-600"
-                    onClick={()=>{
-                        if (i !== currPage) {
-                            setCurrPage(i);
-                        }
-                    }}>{i+1}</button>
+                [...Array(numPages)].map((x, i) => {
+                    if (i !== currPage) {
+                        return <button key={i} className="hover:text-blue-600"
+                            onClick={() => {
+                                if (i !== currPage) {
+                                    setCurrPage(i);
+                                }
+                            }}>{i + 1}</button>
+                    } else {
+                        return <button key={i} className="text-blue-600"
+                        >{i + 1}</button>
+                    }
+                }
+
+
                 )
             }
         </div>
