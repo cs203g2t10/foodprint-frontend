107b58bf05acbcdde5c345f7767d3e5e03ed5efd
michtyx <mxchxlle@gmail.com>
2021-10-12 17:05:31 +0800

    forgot password page

--- src/pages/RequestResetPwd.js
+++ src/pages/RequestResetPwd.js
@@ -26,9 +26,9 @@ const ResetPwd = () => {
         <div className="bg-yellow-standard h-screen">
             <div className="flex justify-center object-contain">
                 <div className="h-auto mt-36 bg-white-standard w-10/12 md:w-6/12 grid md:grid-cols-2 shadow-xxl rounded-xxl">
-                    <div className="ml-16 mt-16">
+                    <div className="ml-16 mt-12">
                         <h1 className = "text-3xl font-bold mb-2 text-green-standard">Gold Fish Brain?</h1>
-                        <h2 className = "text-grey-lighter mb-5 text-sm">Enter the email associated with your account and we’ll send an email with instructions to reset your password</h2>
+                        <h2 className = "text-grey-lighter mb-5 text-sm">Enter the email associated with your account and we’ll send an email with instructions to reset your password.</h2>
                         <div className = "mb-4">
                             <input className="focus:outline-none px-4 py-1 my-1 h-10 rounded-full border border-grey-lightest md:w-11/12"
                                 placeholder="test@example.com"
@@ -40,7 +40,7 @@ const ResetPwd = () => {
                             <button className="rounded-xl px-5 bg-green-standard text-white-standard shadow-sm hover:shadow-md text-justify h-8" disabled={!validateForm}
                                 onClick={requestResetPwd}>Reset Password</button>
                         </div>
-
+                        
                         {
                             (successfulReq ? <div className="text-green-standard pb-4 mt-4">We have sent you an email!</div> : <div className=""></div>)
                         }
