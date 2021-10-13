01570ed67ff59c1dde58bc21997ea6fd816133b5
Daryl Wong <youcanfinddaryl@gmail.com>
2021-10-13 10:32:11 +0800

    Error handling for invalid log in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,9 +9,10 @@ import { useAppContext } from '../lib/contextLib';
 
 const Login = () => {
     const { isAuthenticated, userHasAuthenticated } = useAppContext();
-    
+
     const [email, setEmail] = useState(window.sessionStorage.getItem("email"))
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("");
 
     const validateForm = () => {
         return email > 0 && password > 0;
@@ -23,13 +24,18 @@ const Login = () => {
                 console.log(response.data.token)
                 window.sessionStorage.setItem("token", response.data.token)
                 userHasAuthenticated(true);
+            } else if (response.data.status === "INCORRECT") {
+                console.log("Incorrect input")
+                setError("Please enter a valid username and password!")
+            } else {
+                setError("Unexpected error occured. Please try again.")
             }
         })
     }
-    
+
     if (isAuthenticated) {
         return (
-            <Redirect to = "/"/>
+            <Redirect to="/" />
         )
     }
 
@@ -38,9 +44,9 @@ const Login = () => {
             <div className="flex justify-center">
                 <div className="mt-32 bg-white-standard w-10/12 md:w-7/12 grid md:grid-cols-2 shadow-xxl shadow rounded-xxl">
                     <div className="ml-16 mt-16">
-                        <h1 className = "text-3xl font-bold mb-2 text-green-standard">Log In</h1>
-                        <h2 className = "text-grey-lighter mb-5">Reduce food waste now!</h2>
-                        <div className = "mb-4">
+                        <h1 className="text-3xl font-bold mb-2 text-green-standard">Log In</h1>
+                        <h2 className="text-grey-lighter mb-5">Reduce food waste now!</h2>
+                        <div className="mb-4">
                             <input className="focus:outline-none px-4 py-1 my-1 h-10 rounded-full border border-grey-lightest md:w-11/12"
                                 placeholder="test@example.com"
                                 type="email"
@@ -54,16 +60,28 @@ const Login = () => {
                                 onChange={e => setPassword(e.target.value)} />
                         </div>
 
-                        <div className="pt-5 flex">
+                        
+
+                        <div className="flex pt-5">
                             <button className="rounded-xl px-5 mr-5 bg-green-standard shadow-sm hover:shadow-md text-white-standard text-justify h-8" disabled={!validateForm}
                                 onClick={userLogin}>Log in</button>
                             <Link to="/forgotpassword" className="mx-2 py-1 px-4 self-end border shadow-sm hover:shadow-md rounded-full border-green-standard text-green-standard">Forgot Password?</Link>
                         </div>
+
+                        {
+                            (error ? <div className="py-2">{error}</div> : <></>)
+                        }
+
+                        
                     </div>
 
+
+
                     <div>
-                        <img className = "mt-16 my-7 transform scale-90" src="/images/login.png" alt="log in illustration" />
+                        <img className="mt-16 my-7 transform scale-90" src="/images/login.png" alt="log in illustration" />
                     </div>
+
+
                 </div>
             </div>
         </div>
