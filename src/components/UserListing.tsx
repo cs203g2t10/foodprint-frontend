b11da46c161f732902d850f525a8574740e9dfae
yvonnelhs <yvonnefghijk@gmail.com>
2021-10-15 01:24:15 +0800

    edit user management page

--- src/components/UserListing.tsx
+++ src/components/UserListing.tsx
@@ -22,8 +22,8 @@ const UserListing = (props: any) => {
     }
 
     return (
-        <div className="mx-10 grid grid-cols-12 gap-x-6">
-            <button className="border px-2 rounded bg-red-standard text-white-standard text-center" onClick={() => { deleteUser(props.id) }}>X</button>
+        <div className="mx-10 grid grid-cols-11 gap-x-6">
+            <button className="px-2 rounded-lg bg-red-standard text-white-standard text-center" onClick={() => { deleteUser(props.id) }}>X</button>
             <div>{props.id}</div>
             {(edit ? <>
                 <input className=" border pl-2 rounded col-span-3 focus:outline-none" onChange={(e) => { setEmail(e.target.value) }} value={email} />
@@ -50,7 +50,7 @@ const UserListing = (props: any) => {
                     <p className="col-span-1">{firstName}</p>
                     <p className="col-span-1"> {lastName}</p>
                     <p className="col-span-2">{roles}</p>
-                    <button className="border px-2 rounded bg-green-standard text-white-standard text-center hover:shadow"
+                    <button className="border px-2 rounded-lg bg-green-standard text-white-standard text-center hover:shadow"
                         onClick={() => setEdit(true)}>Edit</button>
                 </>)}
 
