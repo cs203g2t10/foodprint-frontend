14f0bc07cc41e44220be516e2f647f699d9ff714
yvonnelhs <yvonnefghijk@gmail.com>
2021-10-24 23:59:30 +0800

    restricted error page

--- src/components/errors/Restricted.tsx
+++ src/components/errors/Restricted.tsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 const Restricted = () => {
     return (
         <div className="min-h-screen">
-            <div className="grid justify-center items-center gap-y-2 m-10 rounded-xxl shadow lg:mx-64 pb-10 bg-white-dirtyWhite ">
-                <h1 className=" flex text-5xl pt-12 text-green-standard font-bold mx-auto">Restricted access</h1>
-                <h1 className=" flex text-md mb-2 text-grey-standard font-light mx-auto">You are not authenticated or not authorized to access this resource</h1>
-                <Link className="text-white-standard bg-green-standard px-3 py-1 rounded-xl shadow-md hover:shadow-lg text-center w-48 mx-auto" to="/">Return to Home</Link>
+            <div className="m-10 mt-24 lg:mx-64 pb-10 ">
+                <img className = "my-0 flex mx-auto"style = {{height:300}}src="/images/restricted.png" alt="403" />
+                <h1 className="text-5xl pt-12 text-green-standard font-bold text-center">Restricted access</h1>
+                <h1 className="text-md mb-2 text-grey-standard font-light text-center">You are not authenticated or not authorized to access this resource</h1>
+                <Link className="mt-3 text-center text-white-standard bg-green-standard py-1 px-5 flex mx-auto rounded-large shadow-sm hover:shadow-md w-max bg-opacity-80 hover:bg-opacity-100" to="/">Return to Home</Link>
             </div>
         </div>
     )
