0050cd94c7499c527a534fa681478ec74b45a1f3
Daryl Wong <youcanfinddaryl@gmail.com>
2021-10-28 09:47:37 +0800

    change food pic image drop

--- src/components/ChangeFoodPicModal.tsx
+++ src/components/ChangeFoodPicModal.tsx
@@ -1,28 +1,83 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone';
 import ReactModal from 'react-modal'
 
 const ChangeFoodPicModal = (props: any) => {
 
     const { changePic, setChangePic, name } = props;
     const [changed, setChanged] = useState(false);
+    // const [modalMessage, setModalMessage] = useState("");
+    // const [error, setError] = useState("");
+
 
     const customStyles = {
         overlay: { zIndex: 1000 }
     };
 
+    const onDrop = useCallback((acceptedFiles: any) => {
+        if (acceptedFiles == null) {
+            alert("No file selected");
+            return;
+        }
+        var file = acceptedFiles[0];
+        console.log(file);
+
+        console.log("Uploading");
+        // const response = VaccinationService.userUploadVaccination(file);
+        // response.then((response) => {
+        //     console.log("Success: ", response.data.reservationId)
+        //     setModalMessage(`Thank you! We have successfully verfied ${response.data.reason}`);
+        //     setModalIsOpened(true);
+        // }).catch((error) => {
+        //     console.log(error.response.data);
+        //     if (error.response.status === 500) {
+        //         console.log('Error:', error.response.data.reason)
+        //         // setModalMessage(`Error while validating certificate - ${error.response.data.reason}`)
+        //         setError(`Error while validating certificate - ${error.response.data.reason}`)
+        //         setModalIsOpened(true)
+        //     };
+        // });
+    }, []);
+
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
     return (
         <ReactModal style={customStyles} isOpen={changePic} className="mt-10 focus:outline-none">
             <div className="grid justify-center items-center gap-y-2 m-10 rounded-xxl shadow lg:mx-64 pb-10 bg-white-dirtyWhite">
-                <h1 className=" flex text-5xl pt-12 text-green-standard font-bold mx-auto">Change {name} Photo</h1>
-                <h1 className=" flex text-md mb-2 text-grey-standard font-light mx-auto">Please drag and drop your file below.</h1>
+                <h1 className=" flex text-5xl pt-12 text-green-standard font-bold mx-auto">{name} Display Picture</h1>
+                <h1 className=" flex text-md mb-2 text-grey-standard font-light mx-auto">Choose a new photo for your food!</h1>
                 <div className="grid grid-cols-2 justify-between pt-5 gap-x-20">
                     <div>
                         <h1 className="text-center pb-5">Original Photo</h1>
                         <img src={props?.url} alt={props?.description} className="rounded-full mb-5 h-48 w-48 flex mx-auto" />
                     </div>
                     <div>
-                        <h1 className="text-center pb-5">New Photo</h1>
-                        <img src="/images/upload.png" alt="drag and drop" className="rounded-full mb-5 h-48 w-48 flex mx-auto" />
+                        {/* <h1 className="text-center pb-5">New Photo</h1> */}
+                        {/* <img src="/images/upload.png" alt="drag and drop" className="rounded-full mb-5 h-48 w-48 flex mx-auto" /> */}
+                        <div {...getRootProps()}>
+                            <div className="bg-grey-lightest place-items-stretch rounded-xxl text-center p-6 border-dashed border-2 border-grey-light hover:shadow-md shadow-sm focus:outline-none">
+                                <div>
+                                    <input {...getInputProps()} />
+                                    {
+                                        isDragActive ?
+                                            <>
+                                                <h1 className="text-light">Drop your certificate (.oa) here.</h1>
+                                                <img src="/images/cloud.png" alt="cloud illustration" className="transform scale-75 grid justify-items-center mx-auto" />
+                                                <h1 className="text-lightest text-sm pb-2">&nbsp;</h1>
+                                                <h1 className="text-light">&nbsp;</h1>
+                                            </>
+                                            :
+                                            <>
+                                                <h1 className="text-light text-base text-grey-dark">Drag and drop the new image here</h1>
+                                                <img src="/images/cloud.png" alt="cloud illustration" className="transform scale-75 grid justify-items-center mx-auto" />
+                                                <h1 className="text-lightest text-sm pb-2">OR</h1>
+                                                <h1 className="text-light bg-green-standard text-white-standard text-sm opacity-70 hover:opacity-80 shadow-sm hover:shadow-md w-4/5 mx-auto py-1 rounded-full px-5">Click to browse for a file</h1>
+                                            </>
+                                    }
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
