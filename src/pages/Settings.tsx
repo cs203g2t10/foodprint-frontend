154b3ce61cfb2c4004be66aaef6ff328d5b1b7dc
michtyx <mxchxlle@gmail.com>
2021-11-09 20:20:33 +0800

    img changes

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -21,17 +21,17 @@ const Settings = () => {
                         <div className="gap-y-2 m-10 rounded-xxl lg:mx-32 pb-10">
                             <div className="grid lg:grid-cols-3 justify-center items-center gap-x-20 mx-10 pt-10 gap-y-7">
                                 <Link to="/userdetailsconfig" className="bg-white-dirtyWhite rounded-xxl shadow hover:shadow-md py-8">
-                                    <img className=" md:m-auto h-44" src="/images/ManageIngredients.png" alt="shop" />
+                                    <img className=" md:m-auto h-44" src="/images/General.png" alt="shop" />
                                     <h1 className="text-center text-md pt-10 text-green-standard">General</h1>
                                     <h1 className="text-center text-base text-grey-standard px-8">Update user details</h1>
                                 </Link>
                                 <Link to="/twofasetup" className="bg-white-dirtyWhite rounded-xxl shadow hover:shadow-md py-8">
-                                    <img className=" md:m-auto h-44" src="/images/ManageRestaurant.png" alt="shop" />
+                                    <img className=" md:m-auto h-44" src="/images/2FA.png" alt="shop" />
                                     <h1 className="text-center text-md pt-10 text-green-standard">2FA Configuration</h1>
                                     <h1 className="text-center text-base text-grey-standard px-8">Update Two-Factor Authentication settings</h1>
                                 </Link>
                                 <Link to="/dashboard" className="bg-white-dirtyWhite rounded-xxl shadow hover:shadow-md py-8">
-                                    <img className=" md:m-auto h-44" src="/images/Dashboard.png" alt="shop" />
+                                    <img className=" md:m-auto h-44" src="/images/ChangePwd.png" alt="shop" />
                                     <h1 className="text-center text-md pt-10 text-green-standard">Change Password</h1>
                                     <h1 className="text-center text-base text-grey-standard px-2">Update user password</h1>
                                 </Link>
