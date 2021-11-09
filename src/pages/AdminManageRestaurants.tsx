4db4771b093deb6232fcc202683fba398dad3272
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-09 16:31:49 +0800

    admin restaurant listing

--- src/pages/AdminManageRestaurants.tsx
+++ src/pages/AdminManageRestaurants.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import AdminRestaurantListing from '../components/AdminRestaurantListing'
 import Restricted from '../components/errors/Restricted'
 import { useAppContext } from '../lib/AppContext'
 import LogInService, { UserDetails } from '../services/LogInService'
@@ -50,13 +50,7 @@ const AdminManageRestaurants = () => {
                                     restaurants?.map(
                                         (restaurant: any) => {
                                             return (
-                                                <div className="grid grid-cols-11 gap-x-6 mx-6 ">
-                                                    <img className="col-span-1 h-10 w-10 rounded-full object-cover" src={restaurant.picture?.url} alt={restaurant.restaurantName}></img>
-                                                    <p className="col-span-1 text-lg text-grey-dark">{restaurant.restaurantId}</p>
-                                                    <p className="col-span-3 text-lg text-grey-dark">{restaurant.restaurantName}</p>
-                                                    <Link to={'/managerestaurant/'+restaurant.restaurantId} className="col-span-3 text-lg border border-green-standard text-green-standard text-center h-8 rounded-full">Manage Restaurant</Link>
-                                                    <Link to={'/manageingredient/'+restaurant.restaurantId}  className="col-span-3 text-lg border border-green-standard text-green-standard text-center h-8 rounded-full">Manage Ingredients</Link>
-                                                </div>
+                                                <AdminRestaurantListing {...{restaurant}} />
                                             )
                                         }
                                     )
