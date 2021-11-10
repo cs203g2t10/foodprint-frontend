a765ec7f1d5cd0606ffac5ca5c2ecba3d8d7f307
yvonnelhs <yvonnefghijk@gmail.com>
2021-11-10 22:49:18 +0800

    abt us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -20,6 +20,52 @@ const About = () => {
                     <use xlinkHref="#gentle-wave" x="100" y="-25" fill="rgba(243, 232, 201, 1)" />
                 </g>
             </svg>
+            <div className="grid grid-cols-2">
+                <div>
+                    <img className="transform scale-60" src="/images/story.png" alt="our story" />
+                </div>
+                <div className="mr-56 my-auto">
+                    <h1 className="text-green-standard tracking-widest font-semibold text-xl mb-5">OUR STORY</h1>
+                    <h1 className="text-grey-standard text-base">FoodPrint was started in 2021 by a group of SMU undergraduates who are passionate about using technology to solve persisting problems within our community.
+                        Especially using trying times when COVID-19 hit, we saw that F&B establishments face huge difficulty in managing their inventories, worsening the issue of food wastage.
+                        With this, we began our embarked on a journey to solve this problem and gave rise to  is the FoodPrint you see today. </h1>
+                </div>
+            </div>
+            <div className="mb-44">
+                <h1 className="text-green-standard tracking-widest font-semibold text-xl mb-10 text-center">MEET THE TEAM</h1>
+                <div className="grid grid-cols-6 mx-28">
+                    <div className="col-span-1">
+                        <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3" src="/images/cherie.png" alt="cherie" />
+                        <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Cherie Lim</h1>
+                        <h1 className="text-grey-standard tracking-wider text-sm text-center">Backend Developer</h1>
+                    </div>
+                    <div>
+                        <img className="h-40 w-40 object-cover rounded-full mx-auto  mb-3" src="/images/michelle.jpg" alt="michelle" />
+                        <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Michelle Tan</h1>
+                        <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
+                    </div>
+                    <div>
+                        <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3" src="/images/von.jpg" alt="yvonne" />
+                        <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Yvonne Lim</h1>
+                        <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
+                    </div>
+                    <div>
+                        <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3" src="/images/daryl.jpg" alt="daryl" />
+                        <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Daryl Wong</h1>
+                        <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
+                    </div>
+                    <div>
+                        <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3" src="/images/boss.jpg" alt="boss" />
+                        <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Goh Wen Liang</h1>
+                        <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
+                    </div>
+                    <div>
+                        <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3" src="/images/yixuan.jpg" alt="yixuan" />
+                        <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Leow Yi Xuan</h1>
+                        <h1 className="text-grey-standard tracking-wider text-sm text-center">Backend Developer</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
