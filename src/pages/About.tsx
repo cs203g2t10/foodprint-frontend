0471a038ae7814b59a547dbc97da6de8bf6af40d
yvonnelhs <yvonnefghijk@gmail.com>
2021-11-10 23:03:09 +0800

    link to linkedin!

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import "animate.css"
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import { Link } from 'react-router-dom'
 
 const About = () => {
 
@@ -62,45 +63,45 @@ const About = () => {
                     <img className="transform scale-60" src="/images/story.png" alt="our story" />
                 </motion.div>
                 <motion.div className="my-auto mr-52" ref={ref} initial="hidden" animate={controls} variants={fadeInRight}>
-                        <h1 className="text-green-standard tracking-widest font-semibold text-xl mb-5">OUR STORY</h1>
-                        <h1 className="text-grey-standard text-base">FoodPrint was started in 2021 by a group of SMU undergraduates who are passionate about using technology to solve persisting problems within our community.
-                            Especially using trying times when COVID-19 hit, we saw that F&B establishments face huge difficulty in managing their inventories, worsening the issue of food wastage.
-                            With this, we began our embarked on a journey to solve this problem and gave rise to  is the FoodPrint you see today. </h1>
+                    <h1 className="text-green-standard tracking-widest font-semibold text-xl mb-5">OUR STORY</h1>
+                    <h1 className="text-grey-standard text-base">FoodPrint was started in 2021 by a group of SMU undergraduates who are passionate about using technology to solve persisting problems within our community.
+                        Especially using trying times when COVID-19 hit, we saw that F&B establishments face huge difficulty in managing their inventories, worsening the issue of food wastage.
+                        With this, we began our embarked on a journey to solve this problem and gave rise to  is the FoodPrint you see today. </h1>
                 </motion.div>
             </div>
             <div className="mb-44">
                 <h1 className="text-green-standard tracking-widest font-semibold text-xl mb-10 text-center">MEET THE TEAM</h1>
                 <div className="grid grid-cols-6 mx-28">
-                    <div className="col-span-1">
+                    <a target="_blank" href="https://www.linkedin.com/in/cherie-lim-hui-ting/">
                         <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3 shadow-md hover:shadow-lg" src="/images/cherie.png" alt="cherie" />
                         <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Cherie Lim</h1>
                         <h1 className="text-grey-standard tracking-wider text-sm text-center">Backend Developer</h1>
-                    </div>
-                    <div>
+                    </a>
+                    <a target="_blank" href=" https://www.linkedin.com/in/michelle-tan-2a124a1b9/">
                         <img className="h-40 w-40 object-cover rounded-full mx-auto  mb-3 shadow-md hover:shadow-lg" src="/images/michelle.jpg" alt="michelle" />
                         <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Michelle Tan</h1>
                         <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
-                    </div>
-                    <div>
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/yvonnelhs/">
                         <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3 shadow-md hover:shadow-lg" src="/images/von.jpg" alt="yvonne" />
                         <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Yvonne Lim</h1>
                         <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
-                    </div>
-                    <div>
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/wongrenyidaryl/">
                         <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3 shadow-md hover:shadow-lg" src="/images/daryl.jpg" alt="daryl" />
                         <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Daryl Wong</h1>
                         <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
-                    </div>
-                    <div>
+                    </a>
+                    <a target="_blank" href=" https://www.linkedin.com/in/wenlianggoh/">
                         <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3 shadow-md hover:shadow-lg" src="/images/boss.jpg" alt="boss" />
                         <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Goh Wen Liang</h1>
                         <h1 className="text-grey-standard tracking-wider text-sm text-center">Full Stack Developer</h1>
-                    </div>
-                    <div>
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/yixuan-leow/">
                         <img className="h-40 w-40 object-cover rounded-full mx-auto mb-3 shadow-md hover:shadow-lg" src="/images/yixuan.jpg" alt="yixuan" />
                         <h1 className="text-grey-dark font-semibold tracking-wider text-md text-center">Leow Yi Xuan</h1>
                         <h1 className="text-grey-standard tracking-wider text-sm text-center">Backend Developer</h1>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
