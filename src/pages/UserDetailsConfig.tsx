8813a38700ba09332f7a6fc293d6c11cbdabbc03
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-10 21:21:19 +0800

    padding

--- src/pages/UserDetailsConfig.tsx
+++ src/pages/UserDetailsConfig.tsx
@@ -81,7 +81,7 @@ const UserDetailsConfig = () => {
     return (
         <div className="bg-yellow-standard h-screen ">
             <div className="flex justify-center">
-                <div className="mt-28 mb-10 bg-white-standard w-7/12 grid md:grid-cols-2 shadow-xxl shadow rounded-xxl">
+                <div className="mt-16 mb-10 bg-white-standard w-7/12 grid md:grid-cols-2 shadow-xxl shadow rounded-xxl">
                     <div className="ml-16 my-auto">
                         <h1 className="text-4xl font-semibold mb-2 text-green-standard mt-5">Update Details</h1>
                         <h2 className="text-grey-lighter mb-5">Make changes to fields you wish to update</h2>
