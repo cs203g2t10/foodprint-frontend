6caf28934dc065c6156420f1e87fa9d8c187a29c
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-10 13:22:42 +0800

    edit text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -28,7 +28,7 @@ const About = () => {
                 <div className="grid md:grid-cols-7 gap-4 h-screen px-10 py-20 md:py-0">
                     <div className="border-1 md:col-span-4 md:pl-36">
                         <h1 className="text-left md:pt-36 text-7xl font-semibold tracking-wide text-green-standard leading-12 pb-2 animate__animated animate__fadeIn">Reducing <br /> Food Wastage</h1>
-                        <h1 className="text-base text-grey-standard pr-10 pt-2">Here at FoodPrint, we support to F&B establishments in their journey towards food wastage reduction. We provide an innovative solution to help better manage food inventories, which would be especially helpful during this challenging times.</h1>
+                        <h1 className="text-base text-grey-standard pr-10 pt-2">Here at FoodPrint, we support F&B establishments in their journey towards reducing food wastage. We provide an innovative solution to better manage food inventories, based on the level of demand. This is especially helpful during such unpredictable times, in light of the current pandemic.</h1>
                     </div>
                     <div className="md:col-span-3 pt-24">
                         <img className="transform scale-90" src="/images/landingPage.webp" alt="cooking illustration" />
