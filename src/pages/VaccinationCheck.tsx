7d0b6190bea79dd754d002884d84dc90759b0b47
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-10 20:51:45 +0800

    vaccination check vaccination check

--- src/pages/VaccinationCheck.tsx
+++ src/pages/VaccinationCheck.tsx
@@ -1,14 +1,25 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Modal from 'react-modal';
 import { AiOutlineClose } from 'react-icons/ai';
 import VaccinationService from '../services/VaccinationService';
 import { useDropzone } from 'react-dropzone';
+import LogInService, { UserDetails } from '../services/LogInService';
+import { Link } from 'react-router-dom';
 
 const VaccinationCheck = () => {
 
+    const [isVaccinated, setVaccinated] = useState(false);
     const [modalIsOpened, setModalIsOpened] = useState(false);
     const [modalMessage, setModalMessage] = useState("");
     const [error, setError] = useState("");
+    
+
+    useEffect(() => {
+        const userInfo: UserDetails = LogInService.getUserDetails();
+        if (userInfo.vaccinationName !== undefined) {
+            setVaccinated(true);
+        }
+    }, [])
 
     const onDrop = useCallback((acceptedFiles: any) => {
         if (acceptedFiles == null) {
@@ -28,7 +39,6 @@ const VaccinationCheck = () => {
             console.log(error.response.data);
             if (error.response.status === 500) {
                 console.log('Error:', error.response.data.reason)
-                // setModalMessage(`Error while validating certificate - ${error.response.data.reason}`)
                 setError(`Error while validating certificate - ${error.response.data.reason}`)
                 setModalIsOpened(true)
             };
@@ -48,30 +58,41 @@ const VaccinationCheck = () => {
                     <img src="/images/upload.png" alt="upload illustration" className="transform scale-95 pt-8" />
                     <div>
                         <h1 className="text-grey-standard text-5xl font-bold">Upload File.</h1>
-                        <h1 className="text-grey-lighter text-sm pb-4">Dining in is only available for vaccinated individuals. Please upload your certification to help us with this process.</h1>
-                        <div {...getRootProps()}>
-                            <div className="bg-grey-lightest place-items-stretch rounded-xxl text-center p-6 border-dashed border-2 border-grey-light hover:shadow-md shadow-sm focus:outline-none">
-                                <div>
-                                    <input {...getInputProps()} />
-                                    {
-                                        isDragActive ?
-                                            <>
-                                                <h1 className="text-light">Drop your certificate (.oa) here.</h1>
-                                                <img src="/images/cloud.png" alt="cloud illustration" className="transform scale-75 grid justify-items-center mx-auto" />
-                                                <h1 className="text-lightest text-sm pb-2">&nbsp;</h1>
-                                                <h1 className="text-light">&nbsp;</h1>
-                                            </>
-                                            :
-                                            <>
-                                                <h1 className="text-light text-base text-grey-dark">Drag and drop your certificate here</h1>
-                                                <img src="/images/cloud.png" alt="cloud illustration" className="transform scale-75 grid justify-items-center mx-auto" />
-                                                <h1 className="text-lightest text-sm pb-2">OR</h1>
-                                                <h1 className="text-light bg-green-standard text-white-standard text-sm opacity-70 hover:opacity-80 shadow-sm hover:shadow-md w-4/5 mx-auto py-1 rounded-full px-5">Click to browse for a file</h1>
-                                            </>
-                                    }
+                        {
+                            isVaccinated ? <div>
+                                <h1 className="text-grey-lighter text-sm pb-4">Your vaccination status is verified!</h1>
+                                <Link to="/" className="bg-green-standard text-white-standard py-1 px-6 rounded-full">Return Home</Link>
+                            </div>
+                                :
+                                <h1 className="text-grey-lighter text-sm pb-4">Dining in is only available for vaccinated individuals. Please upload your certification to help us with this process.</h1>
+                        }
+                        {
+                            !isVaccinated &&
+                            <div {...getRootProps()}>
+                                <div className="bg-grey-lightest place-items-stretch rounded-xxl text-center p-6 border-dashed border-2 border-grey-light hover:shadow-md shadow-sm focus:outline-none">
+                                    <div>
+                                        <input {...getInputProps()} />
+                                        {
+                                            isDragActive ?
+                                                <>
+                                                    <h1 className="text-light">Drop your certificate (.oa) here.</h1>
+                                                    <img src="/images/cloud.png" alt="cloud illustration" className="transform scale-75 grid justify-items-center mx-auto" />
+                                                    <h1 className="text-lightest text-sm pb-2">&nbsp;</h1>
+                                                    <h1 className="text-light">&nbsp;</h1>
+                                                </>
+                                                :
+                                                <>
+                                                    <h1 className="text-light text-base text-grey-dark">Drag and drop your certificate here</h1>
+                                                    <img src="/images/cloud.png" alt="cloud illustration" className="transform scale-75 grid justify-items-center mx-auto" />
+                                                    <h1 className="text-lightest text-sm pb-2">OR</h1>
+                                                    <h1 className="text-light bg-green-standard text-white-standard text-sm opacity-70 hover:opacity-80 shadow-sm hover:shadow-md w-4/5 mx-auto py-1 rounded-full px-5">Click to browse for a file</h1>
+                                                </>
+                                        }
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        }
+
                     </div>
                 </div>
             </div>
