5505b3f7c882a2ad67d585e2f2e4b63e99f10520
yvonnelhs <yvonnefghijk@gmail.com>
2021-11-10 20:08:34 +0800

    fav restaurant dto

--- src/components/FavouriteRestaurant.tsx
+++ src/components/FavouriteRestaurant.tsx
@@ -14,7 +14,7 @@ const FavouriteRestaurant = (props: any) => {
 
     return (
         <div className="bg-white-offWhite mb-3 grid grid-cols-5 py-2 px-4 rounded-large">
-            <img className="shadow-sm h-12 w-12 object-fill rounded-lg col-span-1 ml-2" src={props.url} alt="restaurant" />
+            <img className="shadow-sm h-12 w-12 object-fill rounded-lg col-span-1 ml-2" src={props.url} alt="pic" />
             <h1 className="text-base col-span-3 my-auto grid pl-5">{props.name}</h1>
             <AiFillHeart className="text-xl my-auto grid text-red-standard opacity-90 hover:opacity-100" onClick={() => { deleteFavourite(props.restaurantId); setDeleteMessage("restaurant " + props.name + " has been unfavourited"); }} />
         </div>
