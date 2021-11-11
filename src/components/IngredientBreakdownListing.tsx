a6ffa08b4a22471075c6f50354a29e00c3ce4577
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-12 02:22:28 +0800

    remove ingredient pic

--- src/components/IngredientBreakdownListing.tsx
+++ src/components/IngredientBreakdownListing.tsx
@@ -2,8 +2,8 @@ const IngredientBreakdownListing = (props: any) => {
 
     return (
         <div className="bg-white-standard rounded-large grid grid-cols-6 mb-3">
-            <img className="h-12 w-12 mx-4 my-2 object-cover rounded-lg col-span-1 " src="/images/forkspoon.jpg" alt="fork spoon"/>
-            <h1 className="my-auto text-base col-span-3">{props.ingredient}</h1>
+            <div className="col-span-1 h-12 my-2"></div>
+            <h1 className="my-auto text-base col-span-3 ">{props.ingredient}</h1>
             <h1 className="my-auto text-base col-span-1">{props.quantity}</h1>
             <h1 className="my-auto text-base col-span-1">{props.units}</h1>
         </div>
