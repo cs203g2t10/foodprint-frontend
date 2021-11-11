28c9cc83c1847b72e0d379aa66c5d1b7118e8b4d
Daryl Wong <youcanfinddaryl@gmail.com>
2021-11-11 12:13:31 +0800

    responsive create user modal

--- src/components/CreateUserModal.tsx
+++ src/components/CreateUserModal.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { AiOutlineClose } from 'react-icons/ai';
 import Modal from 'react-modal';
 import AdminService from '../services/AdminService';
 
@@ -54,28 +55,29 @@ const CreateUserModal = (
     }
 
     return (
-        <Modal style={customStyles} isOpen={createUser} className="mt-24 focus:outline-none">
-            <div className="grid justify-center items-center gap-y-2 m-8 rounded-xxl shadow lg:mx-72 pb-10 bg-white-dirtyWhite">
+        <Modal style={customStyles} isOpen={createUser} className="flex md:mt-24 focus:outline-none">
+            <div className="grid justify-center items-center gap-y-2 rounded-lg shadow pb-10 bg-white-dirtyWhite relative mx-auto px-20">
+                <button className="absolute top-3 right-3 " onClick={() => setCreateUser(false)}> <AiOutlineClose className="h-5 w-5" /> </button>
                 <h1 className=" flex text-5xl pt-10 text-green-standard font-bold mx-auto">Create User</h1>
                 <h1 className=" flex text-base mb-2 text-grey-standard font-light mx-auto">Please fill up all details below </h1>
-                <div className="grid gap-y-5 grid-cols-2 mt-10 gap-x-10">
-                    <div className="flex gap-x-2 justify-between">
+                <div className="grid gap-y-5 md:grid-cols-2 md:mt-10 gap-x-10">
+                    <div className="md:flex gap-x-2 justify-between">
                         <div>Email: </div>
                         <input type="email" className="focus:outline-none px-4 py-1 rounded-large shadow-sm" value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
                     </div>
-                    <div className="flex gap-x-2 justify-between">
+                    <div className="md:flex gap-x-2 justify-between">
                         <div>Password: </div>
                         <input type="password" className="focus:outline-none px-4 py-1 rounded-large shadow-sm" value={password} onChange={(e) => { setPassword(e.target.value) }}></input>
                     </div>
-                    <div className="flex gap-x-2 justify-between">
+                    <div className="md:flex gap-x-2 justify-between">
                         <div>First Name: </div>
                         <input className="focus:outline-none px-4 py-1 rounded-large shadow-sm" value={firstName} onChange={(e) => { setFirstName(e.target.value) }}></input>
                     </div>
-                    <div className="flex gap-x-2 justify-between">
+                    <div className="md:flex gap-x-2 justify-between">
                         <div>Last Name: </div>
                         <input className="focus:outline-none px-4 py-1 rounded-large shadow-sm" value={lastName} onChange={(e) => { setLastName(e.target.value) }}></input>
                     </div>
-                    <div className="flex gap-x-2 justify-between">
+                    <div className="md:flex gap-x-2 justify-between">
                         <div>Role: </div>
                         <input className="focus:outline-none px-4 py-1 rounded-large shadow-sm" value={roles} onChange={(e) => { setRoles(e.target.value) }}></input>
                     </div>
@@ -102,16 +104,16 @@ const CreateUserModal = (
                                     onClick={() => { setCreateUser(false) }}>Return</button>
                             </div>
                         </> :
-                        <div className="pt-2 pb-0 grid grid-cols-2 gap-x-10 justify-center mx-28">
+                        <div className="pt-2 pb-0 grid grid-cols-2 gap-x-10 md:justify-center md:mx-28 gap-y-2">
                             <button className="text-white-standard bg-green-standard px-3 py-1 rounded-xl shadow-sm hover:shadow-md"
                                 onClick={() => createNewUser(email, firstName, lastName, password, roles)}>
-                                    <span>
-                                        {
-                                            loading ? 
+                                <span>
+                                    {
+                                        loading ?
                                             <div className="spinner" id="spinner" />
                                             : 'Confirm'
-                                        }
-                                    </span>
+                                    }
+                                </span>
                             </button>
                             <button className="text-green-standard px-3 py-1 rounded-xl shadow-sm hover:shadow-md border border-green-standard" onClick={() => setCreateUser(false)}>Cancel</button>
                         </div>)
