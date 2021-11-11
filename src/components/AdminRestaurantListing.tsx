62648a2e4aeb29d711b49815629200856ff745e1
yvonnelhs <yvonnefghijk@gmail.com>
2021-11-11 16:49:22 +0800

    standerdise manage pages

--- src/components/AdminRestaurantListing.tsx
+++ src/components/AdminRestaurantListing.tsx
@@ -4,14 +4,14 @@ const AdminRestaurantListing = (props:any) => {
     const {restaurant} = props;
 
     return (
-        <div className="grid grid-cols-8 gap-x-6 mx-6 bg-white-standard rounded-large py-2 px-4">
+        <div className="grid grid-cols-8 gap-x-6 mx-6  rounded-large py-2 px-4">
             {
                 restaurant.picture ? 
                 <img className="col-span-1 h-12 w-12 rounded-lg shadow-sm object-cover" src={restaurant.picture?.url} alt={restaurant.restaurantName} />
                 : <div className="col-span-1 border rounded-full h-10 w-10"/>
             }
-            <p className="col-span-1 text-base text-grey-dark my-auto grid">{restaurant?.restaurantId}</p>
-            <p className="col-span-2 text-base text-grey-dark my-auto grid">{restaurant?.restaurantName}</p>
+            <p className="col-span-1 text-base text-grey-standard my-auto grid">{restaurant?.restaurantId}</p>
+            <p className="col-span-2 text-base text-grey-standard my-auto grid">{restaurant?.restaurantName}</p>
             <Link to={'/managerestaurant/' + restaurant.restaurantId} className="my-auto grid col-span-2 text-base border border-green-standard text-green-standard text-center h-8 rounded-full opacity-90 shadow-sm hover:shadow-md hover:opacity-100">Manage Restaurant</Link>
             <Link to={'/manageingredients/' + restaurant.restaurantId} className="my-auto grid col-span-2 text-base border border-green-standard text-green-standard text-center h-8 rounded-full opacity-90 shadow-sm hover:shadow-md hover:opacity-100">Manage Ingredients</Link>
         </div>
